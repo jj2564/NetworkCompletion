ab5b50c5a9e68530e45c5dcd00eecec1311683e0
IrvingHuang <S409896@sinyi.com.tw>
2019-12-26 13:55:12 +0800

    Initial Commit

---
+++ NetworkCompletion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkCompletion
+//
+//  Created by IrvingHuang on 2019/12/26.
+//  Copyright © 2019 Irving Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
